bb029f9ddc1b9b535217205bdb85b012531f3618
Alfred-Jijo <alfredjijo28@gmail.com>
2024-04-27 16:53:37 +0100

    shoudl be good to publish

---
+++ gitprofile.config.ts
@@ -0,0 +1,159 @@
+// gitprofile.config.ts
+
+const CONFIG = {
+  github: {
+    username: 'Alfred Jijo', // Your GitHub org/user name. (This is the only required config)
+  },
+  /**
+   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/Alfred-Jijo/Alfred-Jijo.github.io, set base to '/'.
+   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
+   * for example your repository is at https://github.com/Alfred-Jijo/portfolio, then set base to '/portfolio/'.
+   */
+  base: '/',
+  projects: {
+    github: {
+      display: true, // Display GitHub projects?
+      header: 'Github Projects',
+      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      automatic: {
+        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
+        limit: 8, // How many projects to display.
+        exclude: {
+          forks: false, // Forked projects will not be displayed if set to true.
+          projects: [], // These projects will not be displayed. example: ['Alfred-Jijo/my-project1', 'Alfred-Jijo/my-project2']
+        },
+      },
+      manual: {
+        // Properties for manually specifying projects
+        projects: ['Alfred-Jijo/Flashcards', 'Alfred-Jijo/Client-Server'], // List of repository names to display. example: ['Alfred-Jijo/my-project1', 'Alfred-Jijo/my-project2']
+      },
+    },
+    external: {
+      header: 'My Projects',
+      // To hide the `External Projects` section, keep it empty.
+      projects: [
+        
+      ],
+    },
+  },
+  seo: {
+    title: 'Portfolio of Ariful Alam',
+    description: '',
+    imageURL: '',
+  },
+  social: {
+    linkedin: 'Alfred Jijo',
+    twitter: 'AlfredJijo',
+    // medium: 'Alfred-Jijo',
+    // dev: 'Alfred-Jijo',
+    stackoverflow: '21635232/alfred-jijo', // example: '1/jeff-atwood'
+    skype: '',
+    telegram: '',
+    website: 'https://www.Alfred-Jijo.com',
+    phone: '',
+    email: 'alfredjijo06@gmail.com',
+  },
+  resume: {
+    fileUrl:
+      '', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'C',
+    'C++',
+    'Python',
+    'MySQL',
+    'Git',
+  ],
+  experiences: [
+    
+  ],
+  educations: [
+    
+  ],
+  publications: [
+    
+  ],
+  // Display articles from your medium or dev account. (Optional)
+  blog: {
+    source: 'dev', // medium | dev
+    username: 'Alfred-Jijo', // to hide blog section, keep it empty
+    limit: 2, // How many articles to display. Max is 10.
+  },
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
+  themeConfig: {
+    defaultTheme: 'lofi',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Display the ring in Profile picture
+    displayAvatarRing: true,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
+      'procyon',
+    ],
+
+    // Custom theme, applied to `procyon` theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  // Optional Footer. Supports plain text or HTML.
+  footer: ``,
+
+  enablePWA: true,
+};
+
+export default CONFIG;
